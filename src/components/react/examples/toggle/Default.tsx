9ef8de51d8708b1ad892671d00ff992ee9721a47
kodai3 <k3dai.su3@gmail.com>
2024-03-19 00:14:59 +0900

    feat: 🎸 add toggle component page
    Signed-off-by: kodai3 <k3dai.su3@gmail.com>

---
+++ src/components/react/examples/toggle/Default.tsx
@@ -0,0 +1,12 @@
+import { Toggle } from '@ubie/ubie-ui';
+import { useState, type FC, type ChangeEventHandler, useCallback } from 'react';
+
+export const DefaultExample: FC = () => {
+  const [isChecked, setIsChecked] = useState<boolean>(false);
+
+  const handleChange: ChangeEventHandler<HTMLInputElement> = useCallback((event) => {
+    setIsChecked(event.target.checked);
+  }, []);
+
+  return <Toggle checked={isChecked} onChange={handleChange} />;
+};
