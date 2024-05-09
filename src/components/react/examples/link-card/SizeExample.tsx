a71198724702a0b810ddbc8319c6438da89f3cfd
hiroki.yokouchi <hiroki.yokouchi@dr-ubie.com>
2024-05-09 17:34:54 +0900

    update link card
    Signed-off-by: hiroki.yokouchi <hiroki.yokouchi@dr-ubie.com>

--- src/components/react/examples/link-card/SizeExample.tsx
+++ src/components/react/examples/link-card/SizeExample.tsx
@@ -1,17 +1,19 @@
+import { HospitalIcon } from '@ubie/ubie-icons';
 import { LinkCard, Stack } from '@ubie/ubie-ui';
 import type { FC } from 'react';
 
-const Default: FC = () => {
+export const SizeExample: FC = () => {
   return (
     <Stack spacing="md" alignItems="normal">
+      <LinkCard title="見出しテキスト" href="https://vitals.ubie.life/" size="small" />
+      <LinkCard title="見出しテキスト" href="https://vitals.ubie.life/" icon={HospitalIcon} size="small" />
       <LinkCard
-        title="タイトル"
+        title="見出しテキスト"
+        href="https://vitals.ubie.life/"
+        icon={HospitalIcon}
         description="これはこのカードの遷移先の説明です。これはこのカードの遷移先の説明です。これはこのカードの遷移先の説明です。これはこのカードの遷移先の説明です。これはこのカードの遷移先の説明です。"
-        href="/"
+        size="small"
       />
-      <LinkCard title="タイトル" href="/" />
     </Stack>
   );
 };
-
-export default Default;
