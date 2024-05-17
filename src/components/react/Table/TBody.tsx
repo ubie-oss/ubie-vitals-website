fdd20a28c227e792ba77a24fca8ee6262bd14b0e
hiroki.yokouchi <hiroki.yokouchi@dr-ubie.com>
2024-05-17 17:16:44 +0900

    fix format
    Signed-off-by: hiroki.yokouchi <hiroki.yokouchi@dr-ubie.com>

--- src/components/react/Table/TBody.tsx
+++ src/components/react/Table/TBody.tsx
@@ -1,4 +1,4 @@
-import type { FC, ReactNode } from "react";
+import type { FC, ReactNode } from 'react';
 
 interface Props {
   children: ReactNode;
--- src/components/react/Table/Th.tsx
+++ src/components/react/Table/Th.tsx
@@ -1,12 +1,10 @@
-import styles from "./Th.module.css";
-import type { FC, ReactNode } from "react";
+import styles from './Th.module.css';
+import type { FC, ReactNode } from 'react';
 
 interface Props {
   children: ReactNode;
 }
 
-const Th: FC<Props> = ({ children }) => (
-  <th className={styles.th}>{children}</th>
-);
+const Th: FC<Props> = ({ children }) => <th className={styles.th}>{children}</th>;
 
 export default Th;
