a20391ed17319296a973895b9eea828b0b1ce670
hiroki.yokouchi <hiroki.yokouchi@dr-ubie.com>
2024-05-17 14:39:15 +0900

    Add <MessageHalfModal> Page
    Signed-off-by: hiroki.yokouchi <hiroki.yokouchi@dr-ubie.com>

---
+++ src/components/react/examples/message-half-modal/DefaultExample.tsx
@@ -0,0 +1,32 @@
+import { MessageHalfModal } from '@ubie/ubie-ui';
+import { useState } from 'react';
+import type { FC } from 'react';
+
+export const DefaultExample: FC = () => {
+  const [open, setOpen] = useState(true);
+  return (
+    <>
+      <button
+        type="button"
+        onClick={() => {
+          setOpen(true);
+        }}
+      >
+        open
+      </button>
+      <MessageHalfModal
+        header="Title"
+        open={open}
+        onClose={() => {
+          setOpen(false);
+        }}
+      >
+        <p>
+          Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's
+          standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to
+          make a type specimen book.
+        </p>
+      </MessageHalfModal>
+    </>
+  );
+};
