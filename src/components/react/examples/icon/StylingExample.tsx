100014a675ad6e17f42b3b70b0514d4a51afa9cc
hiroki.yokouchi <hiroki.yokouchi@dr-ubie.com>
2024-06-03 13:15:25 +0900

    add styleing exapmle(<Icon />)
    Signed-off-by: hiroki.yokouchi <hiroki.yokouchi@dr-ubie.com>

---
+++ src/components/react/examples/icon/StylingExample.tsx
@@ -0,0 +1,6 @@
+import { Icon } from '@ubie/ubie-ui';
+import type { FC } from 'react';
+
+export const StylingExample: FC = () => {
+  return <Icon color="alert" icon="AlertIcon" size="2xl" />;
+};
