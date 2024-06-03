d2c28d15f854f0caad17ddf76ebf894c7e0add34
hiroki.yokouchi <hiroki.yokouchi@dr-ubie.com>
2024-06-03 13:15:07 +0900

    use Icon Component
    Signed-off-by: hiroki.yokouchi <hiroki.yokouchi@dr-ubie.com>

--- src/components/react/examples/icon/DefaultExample.tsx
+++ src/components/react/examples/icon/DefaultExample.tsx
@@ -1,6 +1,6 @@
-import { UbieIcon } from '@ubie/ubie-icons';
+import { Icon } from '@ubie/ubie-ui';
 import type { FC } from 'react';
 
-const DefaultExample: FC = () => <UbieIcon />;
+const DefaultExample: FC = () => <Icon icon="UbieIcon" />;
 
 export default DefaultExample;
