b69b9ff81c872f9cd67c6b5b49563cc2e4709781
takanorip <takanori0170@gmail.com>
2024-06-22 17:39:01 +0900

    fix modal style

--- src/components/react/IconList/IconWrapper.tsx
+++ src/components/react/IconList/IconWrapper.tsx
@@ -2,7 +2,7 @@ import { Heading, MessageModal, Stack, Text } from '@ubie/ubie-ui';
 import { useState, type FC, type JSX } from 'react';
 import iconNames from '@metadata/iconNames.json';
 import styles from './IconWrapper.module.css';
-import { Code } from '../Code';
+import { SimpleCode } from '../SimpleCode';
 
 interface Props {
   children: JSX.Element;
@@ -12,12 +12,17 @@ interface Props {
 const IconWrapper: FC<Props> = ({ children, index }) => {
   const [open, setOpen] = useState(false);
 
-  const toUbieIconsStatement = (iconName: string): string => {
-    return `import { ${iconName} } from '@ubie/ubie-icons'
-<${iconName} />`;
+  const toUbieIconsImport = (iconName: string): string => {
+    return `import { ${iconName} } from '@ubie/ubie-icons';`;
   };
 
-  const toUbieUIStatement = (iconName: string): string => {
+  const toUbieIconsComponent = (iconName: string): string => {
+    return `<${iconName} />`;
+  };
+
+  const toUbieUIImport = `import { Icon } from '@ubie/ubie-ui';`;
+
+  const toUbieUIComponent = (iconName: string): string => {
     return `<Icon icon="${iconName}" />`;
   };
 
@@ -31,17 +36,19 @@ const IconWrapper: FC<Props> = ({ children, index }) => {
         </span>
         <span className={styles.name}>{name}</span>
       </button>
-      <MessageModal header="詳細情報" open={open} onClose={() => setOpen(false)}>
+      <MessageModal open={open} onClose={() => setOpen(false)}>
         <div className={styles.modalContent}>
           <span className={styles.modalIcon}>{children}</span>
           <Heading as="h3" size="md">
             {name}
           </Heading>
           <Stack spacing="md">
-            <Text size="sm">Ubie UIを利用している場合はIconコンポーネントから利用します。</Text>
-            <Code lang="tsx">{toUbieUIStatement(name)}</Code>
-            <Text size="sm">Ubie UIを利用していない場合はUbie Iconsを直接インポートします。</Text>
-            <Code lang="tsx">{toUbieIconsStatement(name)}</Code>
+            <Text size="sm">Ubie UIを利用している場合：</Text>
+            <SimpleCode>{toUbieUIImport}</SimpleCode>
+            <SimpleCode>{toUbieUIComponent(name)}</SimpleCode>
+            <Text size="sm">Ubie UIを利用していない場合：</Text>
+            <SimpleCode>{toUbieIconsImport(name)}</SimpleCode>
+            <SimpleCode>{toUbieIconsComponent(name)}</SimpleCode>
           </Stack>
         </div>
       </MessageModal>
